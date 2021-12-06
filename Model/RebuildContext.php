b4d9a9202ef46edc0e09d16be6009ee634e16c51
Infinity Redux <nick@infinityredux.com>
2021-12-06 00:50:02 +0000

    Implementing product rewrite creation.

--- Model/RebuildContext.php
+++ Model/RebuildContext.php
@@ -1,8 +1,13 @@
 <?php
 namespace InfinityRedux\UrlOverride\Model;
+use Magento\Catalog\Model\CategoryRepository;
+use Magento\Catalog\Model\ProductRepository;
 use Magento\Catalog\Model\ResourceModel\Category\Collection as CategoryCollection;
 use /** @noinspection PhpUndefinedClassInspection */
     Magento\Catalog\Model\ResourceModel\Category\CollectionFactory as CategoryCollectionFactory;
+use Magento\Catalog\Model\ResourceModel\Product\Collection as ProductCollection;
+use /** @noinspection PhpUndefinedClassInspection */
+    Magento\Catalog\Model\ResourceModel\Product\CollectionFactory as ProductCollectionFactory;
 use Magento\Eav\Model\ResourceModel\Entity\Attribute as AttributeResource;
 use Magento\Store\Api\Data\StoreInterface;
 use Magento\Store\Model\StoreManagerInterface;
@@ -20,17 +25,27 @@ class RebuildContext
     private StoreManagerInterface $storeManager;
     private UrlRewriteCollectionFactory $rewriteCollectionFactory;
     private CategoryCollectionFactory $categoryCollectionFactory;
+    private CategoryRepository $categoryRepository;
+    private ProductCollectionFactory $productCollectionFactory;
+    private ProductRepository $productRepository;
 
     public function __construct(AttributeResource           $attributeResource,
                                 StoreManagerInterface       $storeManager,
                                 /** @noinspection PhpUndefinedClassInspection */
                                 CategoryCollectionFactory   $categoryCollectionFactory,
+                                CategoryRepository          $categoryRepository,
+                                /** @noinspection PhpUndefinedClassInspection */
+                                ProductCollectionFactory    $productCollectionFactory,
+                                ProductRepository           $productRepository,
                                 /** @noinspection PhpUndefinedClassInspection */
                                 UrlRewriteCollectionFactory $rewriteCollectionFactory)
     {
         $this->attributeResource = $attributeResource;
         $this->storeManager = $storeManager;
         $this->categoryCollectionFactory = $categoryCollectionFactory;
+        $this->categoryRepository = $categoryRepository;
+        $this->productCollectionFactory = $productCollectionFactory;
+        $this->productRepository = $productRepository;
         $this->rewriteCollectionFactory = $rewriteCollectionFactory;
     }
 
@@ -50,6 +65,21 @@ class RebuildContext
     public function getCategoryCollectionFactory(): CategoryCollection
     {
         return $this->categoryCollectionFactory->create();
+    }
+
+    public function getCategoryRepository(): CategoryRepository
+    {
+        return $this->categoryRepository;
+    }
+
+    public function getProductCollection(): ProductCollection
+    {
+        return $this->productCollectionFactory->create();
+    }
+
+    public function getProductRepository(): ProductRepository
+    {
+        return $this->productRepository;
     }
 
     public function getUrlRewriteCollection(): UrlRewriteCollection
